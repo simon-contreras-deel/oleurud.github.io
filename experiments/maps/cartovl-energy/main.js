1bbbd13f492b733af5dcebd05cba95a4dc98407c
Simon Martín <oleurud@gmail.com>
2018-07-06 17:32:17 +0200

    fixing interactivity

--- experiments/maps/cartovl-energy/main.js
+++ experiments/maps/cartovl-energy/main.js
@@ -19,7 +19,7 @@ const renewableEnergySource = new carto.source.SQL(`
     SELECT *
     FROM global_power_plant_database
     WHERE estimated_generation_gwh is not null
-    AND capacity_mw is not null 
+    AND capacity_mw is not null
     AND fuel1 IN ('Hydro', 'Solar', 'Wind', 'Biomass', 'Geothermal')
 `);
 
@@ -27,7 +27,7 @@ const nonRenewableEnergySource = new carto.source.SQL(`
     SELECT *
     FROM global_power_plant_database
     WHERE estimated_generation_gwh is not null
-    AND capacity_mw is not null 
+    AND capacity_mw is not null
     AND fuel1 NOT IN ('Hydro', 'Solar', 'Wind', 'Biomass', 'Geothermal')
 `);
 
@@ -38,8 +38,7 @@ const emptyEnergySource = new carto.source.SQL(`
 `);
 
 
-
-const renewableEnergyViz = new carto.Viz(`
+const vizTemplate = color => `
     @name: $name
     @capacity_mw: $capacity_mw
     @estimated_generation_gwh: $estimated_generation_gwh
@@ -47,23 +46,14 @@ const renewableEnergyViz = new carto.Viz(`
     @totalGeneration: viewportSum($estimated_generation_gwh)
     @totalCapacity: viewportSum($capacity_mw)
 
-    color: ramp($fuel1, TEALGRN)
-    strokeWidth: 0 
-    width: sqrt($capacity_mw) / 3.14
-`);
+    color: ramp($fuel1, ${color})
+    strokeWidth: 0
+    width: sqrt($capacity_mw) / viewportMax(sqrt($capacity_mw)) * 30
+`;
 
-const nonRenewableEnergyViz = new carto.Viz(`
-    @name: $name
-    @capacity_mw: $capacity_mw
-    @estimated_generation_gwh: $estimated_generation_gwh
-    @fuel1: $fuel1
-    @totalGeneration: viewportSum($estimated_generation_gwh)
-    @totalCapacity: viewportSum($capacity_mw)
+const renewableEnergyViz = new carto.Viz(vizTemplate('TEALGRN'));
+const nonRenewableEnergyViz = new carto.Viz(vizTemplate('BURG'));
 
-    color: ramp($fuel1, BURG)
-    strokeWidth: 0 
-    width: sqrt($capacity_mw) / 3.14 /viewportMax(sqrt($capacity_mw)) *50
-`);
 
 
 const nonRenewableEnergyLayer = new carto.Layer('nonRenewableEnergyLayer', nonRenewableEnergySource, nonRenewableEnergyViz);
@@ -75,53 +65,72 @@ renewableEnergyLayer.addTo(map);
 
 
 /* ACTIONS */
-let biggest;
+let featureSelected;
 const delay = 50;
 const interactivity = new carto.Interactivity([renewableEnergyLayer, nonRenewableEnergyLayer]);
 interactivity.on('featureEnter', event => {
-    if (!biggest) {
+    let biggest = featureSelected;
+    if (!featureSelected) {
         biggest = event.features[0];
-        for (let feature of event.features) {
-            if (feature.variables.estimated_generation_gwh.value > biggest.variables.estimated_generation_gwh.value) {
-                biggest = feature;
-            }
-        }
+    } else {
+        resetFeature(featureSelected);
+        featureSelected = null;
+    }
 
-        if (biggest) {
-            document.getElementById('info').innerHTML = `
-                <div class="container">
-                    <h3 class="h3">${biggest.variables.name.value}</h3>
-                    <p>Type: ${biggest.variables.fuel1.value}</p>
-                    <p>Capacity: ${Number(biggest.variables.capacity_mw.value).toFixed(0)} GWh</p>
-                </div>
-            `;
-
-            biggest.color.blendTo('opacity(DeepPink, 0.5)', delay)
-            biggest.strokeWidth.blendTo('4', delay);
-            biggest.strokeColor.blendTo('opacity(DeepPink, 0.8)', delay);
+    for (let feature of event.features) {
+        if (feature.variables.estimated_generation_gwh.value > biggest.variables.estimated_generation_gwh.value) {
+            biggest = feature;
         }
+    }
+
+    if (biggest) {
+        featureSelected = biggest;
+
+        document.getElementById('info').innerHTML = `
+            <div class="container">
+                <h3 class="h3">${biggest.variables.name.value}</h3>
+                <p>Type: ${biggest.variables.fuel1.value}</p>
+                <p>Capacity: ${Number(biggest.variables.capacity_mw.value).toFixed(0)} GWh</p>
+            </div>
+        `;
+
+        biggest.color.blendTo('opacity(DeepPink, 0.5)', delay)
+        biggest.strokeWidth.blendTo('4', delay);
+        biggest.strokeColor.blendTo('opacity(DeepPink, 0.8)', delay);
     }
 })
 
-interactivity.on('featureLeave', () => {
-    if (biggest) {
-        document.getElementById('info').innerHTML = '';
-        biggest.color.reset(delay);
-        biggest.strokeWidth.reset(delay);
-        biggest.strokeColor.reset(delay);
-        biggest = null;
+interactivity.on('featureLeave', event => {
+    if (featureSelected && event.features.length) {
+        for (let feature of event.features) {
+            console.log(feature, featureSelected);
+            if(feature.id === featureSelected.id) {
+                resetFeature(featureSelected);
+                featureSelected = null;
+            }
+        }
     }
 });
 
+function resetFeature(feature) {
+    document.getElementById('info').innerHTML = '';
+    feature.color.reset(delay);
+    feature.strokeWidth.reset(delay);
+    feature.strokeColor.reset(delay);
+}
+
 
 renewableEnergyLayer.on('updated', () => {
+    const renewableEnergy = renewableEnergyViz.variables.totalCapacity.eval() || 0;
+    const nonRenewableEnergy = nonRenewableEnergyViz.variables.totalCapacity.eval() || 0;
+
     document.getElementById('total').innerHTML = `
         <h3>POWER PLANTS</h3>
         <hr>
         <p><strong>Renewable</strong>:</p>
-        <p>${Number(renewableEnergyViz.variables.totalCapacity.eval().toFixed(0)).toLocaleString()} MW</p>
+        <p>${Number(renewableEnergy.toFixed(0)).toLocaleString()} MW</p>
         <p><strong>Non-renewable</strong>:</p>
-        <p>${Number(nonRenewableEnergyViz.variables.totalCapacity.eval().toFixed(0)).toLocaleString()} MW</p>
+        <p>${Number(nonRenewableEnergy.toFixed(0)).toLocaleString()} MW</p>
     `;
 });
 
