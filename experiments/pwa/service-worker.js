b40662d4e627fa503a6931f214207222df3e17f0
Simon Martín <oleurud@gmail.com>
2020-01-04 16:35:11 +0100

    pwa

--- experiments/pwa/service-worker.js
+++ experiments/pwa/service-worker.js
@@ -6,21 +6,33 @@ const precacheResources = [
     'css/main.css',
     'images/space1.jpg',
     'images/space2.jpg',
-    'images/space3.jpg'
+    'images/space3.jpg',
+    'js/jquery-3.3.1.slim.min.js',
+    'js/bootstrap.bundle.min.js'
 ];
 
 self.addEventListener('install', event => {
     console.log('Service worker install event!');
     event.waitUntil(
         caches.open(cacheName)
-            .then(cache => {
-                return cache.addAll(precacheResources);
-            })
+            .then(cache => cache.addAll(precacheResources))
     );
 });
 
 self.addEventListener('activate', event => {
     console.log('Service worker activate event!');
+
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (!cacheName !== key) {
+                    return caches.delete(key);
+                }
+            })
+        )).then(() => {
+            console.log(`${cacheName} now ready to handle fetches!`);
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
